0351af6f39c4cd29bdf26ac2122c745156918663
Valentine <valentinemaillard1@gmail.com>
2020-03-13 18:02:05 -0400

    completed maxChar problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,34 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {}
+
+    for(let char of str){
+        chars[char] = chars[char] + 1 || 1
+    }
+
+   return Object.keys(chars).reduce((a, b) => chars[a] > chars[b] ? a : b)
+}
+
+
+// using the for...in loop solution
+// function maxChar(str){
+//     let chars = {}
+//     let max = 0
+//     let maxChar = ''
+
+//     for(let char of str){
+//       chars[char] = chars[char] + 1 || 1
+//     }
+
+//     for(let char in chars){
+//         if(chars[char] > max){
+//             max = chars[char]
+//             maxChar = char
+//         }
+//     }
+//     return maxChar
+// }
 
 module.exports = maxChar;
