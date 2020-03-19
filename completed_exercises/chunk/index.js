4fe22cb6bfd6138d411d7bb25b9170370e3e7e13
Valentine <valentinemaillard1@gmail.com>
2020-03-19 17:22:19 -0400

    completed chunk problem

--- completed_exercises/chunk/index.js
+++ completed_exercises/chunk/index.js
@@ -9,11 +9,31 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-   let newArr = []
-   for(let i = 0; i < array.length; i += size){
-       newArr.push(array.slice(i, i + size))
-   }
-   return newArr
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
 }
 
 module.exports = chunk;
+
+// function chunk(array, size) {
+//   const chunked = [];
+//
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//
+//   return chunked;
+// }
