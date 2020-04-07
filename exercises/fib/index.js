9004a55d0e42c17fdfa981001b3015deb6f280a5
Valentine <valentinemaillard1@gmail.com>
2020-04-07 13:06:07 -0400

    completed fibonacci prob

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,26 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// for loop solution
+function fib(n) {
+    let arr = [0, 1]
+    for(let i = 0; i <= n; i++){
+    arr.push(arr[i] + arr[i + 1])
+    }
+    return arr[n]
+}
+console.log(fib(6))
+
+// recursive solution
+// if isn't smaller then 2 call fib(n - 1) + fib(n - 2)
+// at the end it will be < 2 and we can add all the fib(n) together to get the result of n 
+// fib(6) will produce 8 fib(n),  fib(4) will produce 3 fib(n)
+// function fib(n) {
+//     return n < 2 ? n : fib(n - 1) + fib(n - 2)
+// }
+
+
+
+
 
 module.exports = fib;
