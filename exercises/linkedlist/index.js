7b816a9247d0f72a63a22ea3a433445f2523a2c5
Valentine <valentinemaillard1@gmail.com>
2020-05-12 12:34:24 -0400

    created the Node class and the LinkedList class with the insertFirst function

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,30 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    // create the node with some data
+    // and points to null as the next node when initialized
+    constructor(data, next = null){
+        this.data = data;
+        this.next = next
+    }
+}
 
-class LinkedList {}
+// const node1 = new Node('123', null)
+class LinkedList {
+    // sets the list with a head of null since it is empty when created
+    constructor(){
+        this.head = null
+    }
+    
+    insertFirst(data) {
+        // create a new node
+        // assigns it as the new head pointing to the old head
+        // this allows to insert a Node inside our list from the beginning of the list
+        this.head = new Node(data, this.head)
+    }
+    
+}
+// const l = new LinkedList(Node)
 
 module.exports = { Node, LinkedList };
